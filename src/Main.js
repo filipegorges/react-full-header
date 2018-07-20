66e61d5de28cacd1196a7152d21e266bec53f528
Filipe Gorges Reuwsaat <lipirr@hotmail.com>
2018-07-20 19:03:12 -0300

    Add full header video component

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { headerStyle, containerStyle, titleStyle, subtitleStyle, videoStyle } from './Styles';
 
-const Component = ({ name }) => (
-    <h1>Hello {name}!</h1>
-);
-
-Component.propTypes = {
-    name: PropTypes.string.isRequired,
+const defaultProps = {
+    bgColor: '#ccc',
+    textColor: '#fff',
+    font: 'sans-serif',
+    bgImg: '',
 };
 
-export default Component;
+const propTypes = {
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
+    font: PropTypes.string,
+    bgImg: PropTypes.string,
+    video: PropTypes.string,
+};
+
+const FullHeader = ({ title, subtitle, bgColor, textColor, font, bgImg, video }) => {
+    const headerStyleCombined = {
+        ...headerStyle,
+        backgroundColor: bgColor,
+        backgroundImage: `url(${bgImg})`,
+        color: textColor,
+        fontFamily: font,
+    }
+
+    const component = (
+        <header style={headerStyleCombined}>
+            <div style={containerStyle}>
+                {title && <h1 style={titleStyle}>{title}</h1>}
+                {subtitle && <h2 style={subtitleStyle}>{subtitle}</h2>}
+            </div>
+            {video && <video style={videoStyle} autoPlay muted loop src={video} />}
+        </header>
+    );
+
+    return component;
+};
+
+
+FullHeader.propTypes = propTypes;
+FullHeader.defaultProps = defaultProps;
+
+export default FullHeader;
--- tests/specs/Main.spec.js
+++ tests/specs/Main.spec.js
@@ -1,22 +1,113 @@
 import React from 'react';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
 import { shallow } from 'enzyme';
-import Component from '../../src/Main';
+import FullHeader from '../../src/Main';
 
-describe('Component Hello', () => {
-   it('should have h1 to display the Hello', () => {
-       const wrapper = shallow(<Component name='lyef' />);
-       expect(wrapper.find('h1')).to.have.length(1);
+chai.use(chaiEnzyme());
+
+describe('<FullHeader />', () => {
+   it('should have header tag when mount', () => {
+        const wrapper = shallow(<FullHeader />);
+        expect(wrapper.find('header')).to.have.length(1);
    });
 
-    it('should have props for name', () => {
-        const wrapper = shallow(<Component name='lyef' />);
-        expect(wrapper.props().name).to.be.defined;
-    });
+   context('title', () => {
+       it('should have h1 tag when title passed', () => {
+           const wrapper = shallow(<FullHeader title="TDD"/>);
+           expect(wrapper.find('h1')).to.have.length(1);
+       });
 
-    it('should create a correct DOM structure', () => {
-        const wrapper = shallow(<Component name='lyef' />);
-        const componentMock = shallow(<h1>Hello lyef!</h1>);
-        expect(wrapper.html()).to.be.equal(componentMock.html());
-    });
+       it('should not have h1 tag when title is not passed', () => {
+           const wrapper = shallow(<FullHeader />);
+           expect(wrapper.find('h1')).to.have.length(0);
+       });
+
+       it('should have h1 tag with the title passed', () => {
+           const wrapper = shallow(<FullHeader title="TDD" />);
+           expect(wrapper.find('h1').props().children).to.be.equal("TDD");
+       });
+   });
+
+   context('subtitle', () => {
+       it('should have h2 when subtitle passed', () => {
+           const wrapper = shallow(<FullHeader subtitle="curso" />);
+           expect(wrapper.find('h2')).to.have.length(1);
+       });
+
+       it('should not have h2 tag when subtitle is not passed', () => {
+           const wrapper = shallow(<FullHeader />);
+           expect(wrapper.find('h2')).to.have.length(0);
+       });
+
+       it('should have h2 with propper text when subtitle is passed', () => {
+           const wrapper = shallow(<FullHeader subtitle="curso" />);
+           expect(wrapper.find('h2').props().children).to.be.equal("curso");
+       });
+   });
+
+   context('bgColor', () => {
+       it('should have background-color equal #ccc when none is passed', () => {
+           const wrapper = shallow(<FullHeader title="TDD" />);
+           expect(wrapper).to.have.style('background-color').equal('#ccc');
+       });
+
+       it('should have background-color equal #000 when passed', () => {
+           const wrapper = shallow(<FullHeader bgColor="#000" />);
+           expect(wrapper).to.have.style('background-color').equal('#000');
+       });
+   });
+
+   context('textColor', () => {
+       it('should have color equal #fff when none is passed', () => {
+           const wrapper = shallow(<FullHeader />);
+           expect(wrapper).to.have.style('color').equal('#fff');
+       });
+
+       it('should have color equal #ff0000 when passed', () => {
+           const wrapper = shallow(<FullHeader textColor="#ff0000"/>);
+           expect(wrapper).to.have.style('color').equal('#ff0000');
+       });
+   });
+
+   context('font', () => {
+       it('should have font equal sans-serif when none is passed', () => {
+           const wrapper = shallow(<FullHeader />);
+           expect(wrapper).to.have.style('font-family').equal('sans-serif');
+       });
+
+       it('should have font equal open-sans when passed', () => {
+           const wrapper = shallow(<FullHeader font="open-sans" />);
+           expect(wrapper).to.have.style('font-family').equal('open-sans');
+       });
+   });
+
+   context('bgImg', () => {
+       it('should have background-image equal when none is passed', () => {
+           const wrapper = shallow(<FullHeader />);
+           expect(wrapper).to.have.style('background-image').equal('url()');
+       });
+
+       it('should have background-image equal bg.jpg when passed', () => {
+           const wrapper = shallow(<FullHeader bgImg="bg.jpg" />);
+           expect(wrapper).to.have.style('background-image').equal('url(bg.jpg)');
+       });
+   });
+
+   context('video', () => {
+       it('should have video tag when video is passed', () => {
+           const wrapper = shallow(<FullHeader video="my_video.mp4"/>);
+           expect(wrapper.find('video')).to.have.length(1);
+       });
+
+       it('should not have video tag when video not is not passed', () => {
+           const wrapper = shallow(<FullHeader />);
+           expect(wrapper.find('video')).to.have.length(0);
+       });
+
+       it('should have video tag with the video is passed', () => {
+           const wrapper = shallow(<FullHeader video="my_video.mp4" />);
+           expect(wrapper.find('video').props().src).to.be.equal('my_video.mp4');
+       });
+   });
 });
